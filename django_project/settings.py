87931a481d51f12dd9793f9dacc37275c8a46432
divyashk <dk0018w@gmail.com>
2020-08-28 17:08:10 +0530

    added a static root in settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -136,6 +136,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
@@ -145,4 +146,4 @@ AWS_S3_REGION_NAME = 'ap-south-1' #change to your region
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
