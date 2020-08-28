4e2697f7810132372743bd99c53e03a12981923f
divyashk <dk0018w@gmail.com>
2020-08-28 16:54:19 +0530

    now using AWS buckets for storing user profile pics

--- users/models.py
+++ users/models.py
@@ -10,13 +10,14 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-    def save(self, **kwargs):
-        super().save();
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    # this image resizing can't be used with AWS buckets
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs);
+    #     img = Image.open(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
 
     def __str__(self):
         return f'{self.user.username} Profile'
